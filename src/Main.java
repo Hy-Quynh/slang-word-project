2a8e4b7f499e0e58594c64b727c17965d004536b
Hy-Quynh <hyquynh123@gmail.com>
2022-07-05 19:57:52 +0700

    find slang

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-	
-	static void renderMenu() {
+	public static void renderMenu() {
 		Scanner keyboard = new Scanner(System.in);
+		SlangWord slangWord = new SlangWord();
+		
 		int option = 0;
 		String[] menu = {"1. Tìm kiếm theo slang word.", "2. Tìm kiếm theo definition.", "3. Hiển thị history.",
 				"4. Hiển thị history.", "5. Thêm 1 slang words.", "6. Sửa 1 slang words.", "7. Xoá 1 slang word.",
@@ -20,6 +21,15 @@ public class Main {
 				System.out.println("Lựa chọn của bạn không có trong menu, mời bạn chọn lại");
 			}
 		}while (option > menu.length || option < 1);    
+		
+		
+		if (option == 1) { 
+			keyboard.nextLine();
+			System.out.print("Mời bạn nhập vào slang world cần tìm kiếm: ");
+			String key  = keyboard.nextLine();
+			String result[][] = slangWord.findSlang(key);
+			System.out.println(result);
+		}
 	}
 	
 	public static void main(String[] args) {
--- src/SlangWord.java
+++ src/SlangWord.java
@@ -1,17 +1,129 @@
+import java.util.TreeMap;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
 public class SlangWord {
+	private TreeMap<String, List<String>> map = new TreeMap<>();
+	private static SlangWord obj = new SlangWord();// Early, instance will be created at load time
+	private int sizeMap;
+	private String FILE_SLANGWORD = "slang.txt";
+	private String FILE_ORIGINAL_SLANGWORD = "slangword-goc.txt";
+	private String FILE_HISTORY = "history.txt";
 	
 	
-	private SlangWord() {
-		
+	SlangWord() {
+		try {
+			String current = new java.io.File(".").getCanonicalPath();
+			System.out.println("Current dir:" + current);
+			FILE_SLANGWORD = current + "//" + FILE_SLANGWORD;
+			FILE_ORIGINAL_SLANGWORD = current + "//" + FILE_ORIGINAL_SLANGWORD;
+			FILE_HISTORY = current + "//" + FILE_HISTORY;
+			readFile(FILE_SLANGWORD);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	private static class SingletonHelper {
-		static final SlangWord INSTANCE = new SlangWord();
-	}
-	
+
 	public static SlangWord getInstance() {
-		return SingletonHelper.INSTANCE;
+		if (obj == null) {
+			synchronized (SlangWord.class) {
+				if (obj == null) {
+					obj = new SlangWord();
+				}
+			}
+		}
+		return obj;
+	}
+	
+	
+	void readFile(String file) throws Exception {
+		map.clear();
+		String slag = null;
+		Scanner scanner = new Scanner(new File(file));
+		scanner.useDelimiter("`");
+		scanner.next();
+		String temp = scanner.next();
+		String[] part = temp.split("\n");
+		int i = 0;
+		int flag = 0;
+		sizeMap = 0;
+		
+		while (scanner.hasNext()) {
+			List<String> meaning = new ArrayList<String>();
+			slag = part[1].trim();
+			temp = scanner.next();
+			part = temp.split("\n");
+			if (map.containsKey(slag)) {
+				meaning = map.get(slag);
+			}
+			if (part[0].contains("|")) {
+				String[] d = (part[0]).split("\\|");
+				for (int ii = 0; ii < d.length; ii++)
+					System.out.println(d[ii]);
+				Collections.addAll(meaning, d);
+				sizeMap += d.length - 1;
+			} else {
+				meaning.add(part[0]);
+			}
+			// map.put(slag.trim(), meaning);
+			map.put(slag, meaning);
+			i++;
+			sizeMap++;
+		}
+		scanner.close();
+	}
+	
+	public String[][] getData() {
+		String s[][] = new String[sizeMap][3];
+		Set<String> slagListSet = map.keySet();
+		Object[] slagList = slagListSet.toArray();
+		int index = 0;
+		
+		for (int i = 0; i < sizeMap; i++) {
+			s[i][0] = String.valueOf(i);
+			s[i][1] = (String) slagList[index];
+			List<String> meaning = map.get(slagList[index]);
+			s[i][2] = meaning.get(0);
+			for (int j = 1; j < meaning.size(); j++) {
+				if (i < sizeMap)
+					i++;
+				s[i][0] = String.valueOf(i);
+				s[i][1] = (String) slagList[index];
+				s[i][2] = meaning.get(j);
+			}
+			index++;
+		}
+		return s;
+	}
+
+	public String[][] findSlang(String key) { 
+		String listData[][]  = getData();
+		String dataMap[][] = new String[sizeMap][3];
+		
+		for (int i = 0; i < listData.length; i++) {
+			if ( listData[i][1].contains(key)) {
+				dataMap[dataMap.length] = listData[i];
+			}
+		}
+		return dataMap;
+	}
+	
+	public String[][] getMeaning(String key) {
+		List<String> listMeaning = map.get(key);
+		if (listMeaning == null)
+			return null;
+		int size = listMeaning.size();
+		String s[][] = new String[size][3];
+		for (int i = 0; i < size; i++) {
+			s[i][0] = String.valueOf(i);
+			s[i][1] = key;
+			s[i][2] = listMeaning.get(i);
+		}
+		return s;
 	}
 	
 	
